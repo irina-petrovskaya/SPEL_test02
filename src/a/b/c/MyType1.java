b01cfbca667ef4f90f7faca8437d7e3381f7c5b0
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2016-04-14 13:21:42 +0300

    Initial commit

---
+++ src/a/b/c/MyType1.java
@@ -0,0 +1,20 @@
+package a.b.c;
+
+public class MyType1 {
+
+    static class InnerType2 {
+            public static String foo(){
+                return "foo";
+            }
+
+        }
+    public static String foo(){
+        return "foo";
+    }
+    static enum InnerEnum1 {
+        EL1,
+        EL2
+    }
+
+
+}
