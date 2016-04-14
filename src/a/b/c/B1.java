0f5942e38a6fe786534b12f728c589d869fbcb8b
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2016-04-14 14:57:02 +0300

    formatting

--- src/a/b/c/B1.java
+++ src/a/b/c/B1.java
@@ -7,19 +7,19 @@ public class B1 {
     private String inner1;
     private String inner2;
 
-    public void setInner1(String inner1) {
-        this.inner1 = inner1;
-    }
-
     public String getInner1() {
         return inner1;
     }
 
-    public void setInner2(String inner2) {
-        this.inner2 = inner2;
+    public void setInner1(String inner1) {
+        this.inner1 = inner1;
     }
 
     public String getInner2() {
         return inner2;
+    }
+
+    public void setInner2(String inner2) {
+        this.inner2 = inner2;
     }
 }
--- src/a/b/c/MyType1.java
+++ src/a/b/c/MyType1.java
@@ -2,18 +2,20 @@ package a.b.c;
 
 public class MyType1 {
 
-    static class InnerType2 {
-            public static String foo(){
-                return "foo";
-            }
-
-        }
-    public static String foo(){
+    public static String foo() {
         return "foo";
     }
+
     static enum InnerEnum1 {
         EL1,
         EL2
+    }
+
+    static class InnerType2 {
+        public static String foo() {
+            return "foo";
+        }
+
     }
 
 
